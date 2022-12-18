5fce7860fd39ecfb220d6aee17d0c83a982cc614
rasid17 <rasid457@gmail.com>
2022-12-18 23:55:24 +0700

    fix character and voice names

--- lib/screens/movie_detail/voice_actor/voice_actor.dart
+++ lib/screens/movie_detail/voice_actor/voice_actor.dart
@@ -153,47 +153,69 @@ class _VoiceActorDetailScreenState extends State<VoiceActorDetailScreen> {
               ),
               const SizedBox(height: 10),
               Container(
-                height: 100,
+                height: 200,
                 child: ListView.builder(
                   physics: const BouncingScrollPhysics(),
                   scrollDirection: Axis.horizontal,
                   shrinkWrap: true,
                   itemCount: getData('voices', def: []).length,
                   itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) {
-                              return CharacterDetailScreen(
-                                id: getData('voices', def: [])[index]
-                                        ?['character']?['mal_id'] ??
-                                    1,
-                                image: getData('voices', def: [])[index]
-                                            ?['character']?['images']?['jpg']
-                                        ?['image_url'] ??
-                                    '',
-                              );
-                            },
+                    return Column(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (context) {
+                                  return CharacterDetailScreen(
+                                    id: getData('voices', def: [])[index]
+                                            ?['character']?['mal_id'] ??
+                                        1,
+                                    image: getData('voices', def: [])[index]
+                                                ?['character']?['images']
+                                            ?['jpg']?['image_url'] ??
+                                        '',
+                                  );
+                                },
+                              ),
+                            );
+                          },
+                          child: Container(
+                            margin: const EdgeInsets.only(right: 10),
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            clipBehavior: Clip.hardEdge,
+                            child: Image.network(
+                              getData('voices', def: [])[index]['character']
+                                      ?['images']?['jpg']?['image_url'] ??
+                                  '',
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        );
-                      },
-                      child: Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        width: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(10),
                         ),
-                        clipBehavior: Clip.hardEdge,
-                        child: Image.network(
-                          getData('voices', def: [])[index]['character']
-                                  ?['images']?['jpg']?['image_url'] ??
-                              '',
-                          fit: BoxFit.cover,
+                        Container(
+                          width: 100,
+                          height: 60,
+                          child: Column(
+                            children: [
+                              Text(
+                                getData('voices', def: [])[index]['character']
+                                        ?['name'] ??
+                                    '',
+                                style: const TextStyle(
+                                  fontSize: 12,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
+                      ],
                     );
                   },
                 ),
